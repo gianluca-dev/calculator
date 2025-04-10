4dad79761981c824af549d49319ff09d24e9599d
Gian-Luca Loy <gianluca.loy.dev@gmail.com>
2025-04-10 19:05:44 +0200

    Update Version_1.1

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # calculator
+
+A simple and user-friendly calculator built with HTML, CSS and JavaScript.
+
+## Features
+- Supports basic arithmetic operations (addition, subtraction, multiplication, division).
+- Real-time display of input and calculation results.
+- Error handling for invalid inputs.
+- Clear Button to reset inputs and the display.
+
+### Technologies
+- HTML
+- CSS
+- JavaScript
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,40 +1,48 @@
+// Declare variables for user input, display output, and input buffer
 let input = '';
 let displayInput = '';
 let inputBuffer = '';
 
 const container = document.getElementById('key-container');
-
+// Add event listener to button container, detecting clicks on calculator buttons
 container.addEventListener('click', (event) => {
     const key = event.target;
     if (key.classList.contains('key')) {
+        // Process the clicked button's input
         getInput(key.textContent);
     }
 });
-
+// Function to handle button input for both display and calculation logic
 function getInput(key) {
+    // Update display with the entered key
     displayInput += key;
-
+    // Convert special characters for proper calculation
     if (key === '×') {
         input += '*';
     } else if (key === '÷') {
         input += '/';
     } else {
+        // Append other valid input characters
         input += key;
     }
-
+    // Update display element
     document.getElementById('display').innerText = displayInput;
 }
 
 document.getElementById('equation').addEventListener('click', calculate);
-
+// Function to perform calculations based on user input
 function calculate() {
     try {
+        // Evaluate the mathematical expression and display the result
         let result = eval(input);
         document.getElementById('display').innerText = result;
+        // Store the result for further calculations
         input = result.toString();
         displayInput = result.toString();
     } catch (error) {
+        // Handle invalid input by showing an error message and resetting variables
         console.log('Invalid Input! ' + error);
+        // Display "Error" on invalid input
         document.getElementById('display').innerText = 'Error';
         input = '';
         displayInput = '';
@@ -42,7 +50,7 @@ function calculate() {
 }
 
 document.getElementById('clear').addEventListener('click', clear);
-
+// Function to clear all stored inputs and reset the display
 function clear() {
     input = '';
     displayInput = '';
